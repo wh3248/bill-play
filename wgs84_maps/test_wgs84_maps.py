00cea35193a7f8784e61108feb248f3b5cccbcc1
wh3248 <wh3248.princeton.edu>
2022-05-11 09:44:41 -0400

    add wgs84_maps

---
+++ wgs84_maps/test_wgs84_maps.py
@@ -0,0 +1,52 @@
+import json
+import time
+from wgs84_maps import WGS84Mapping
+
+def read_bounds(domain_state_path):
+    """
+        Read the bounds from the state in a hydrogen domain directory.
+        Returns (grid_bounds, wgs84_bounds, conus).
+        The value of conus is either 'conus1' or 'conus2' depending on the domain.
+        The grid_bounds in (min_x, min_y, max_x, max_y) in conus grid coordinates.
+        The wgs84_bounds is (min_lon, min_lat, max_lon, max_lat) in WGS84 coords
+    """
+
+    grid_bounds = None
+    wgs84_bounds = None
+    conus = None
+    with open(domain_state_path, "r") as stream:
+        contents = stream.read()
+        domain_state = json.loads(contents)
+        grid_bounds = domain_state.get("grid_bounds")
+        wgs84_bounds = domain_state.get("wgs84_bounds")
+        shape_regions = domain_state.get("shape_regions")
+        conus = shape_regions[0].get("shape_source")
+    return (grid_bounds, wgs84_bounds, conus)
+
+def compare_domain_answer(domain_state_path):
+    """Compare the grid bounds from the domain_path with the computed bounds."""
+
+    (grid_bounds, wgs84_bounds, conus) = read_bounds(domain_state_path)
+    print(wgs84_bounds)
+    print(grid_bounds)
+    start_time = time.time()
+    wgs84_mapping = WGS84Mapping()
+    wgs84_mapping.load_maps(conus)
+    (x1, y1) = wgs84_mapping.map_to_grid(wgs84_bounds[0], wgs84_bounds[1])
+    (x2, y2) = wgs84_mapping.map_to_grid(wgs84_bounds[2], wgs84_bounds[3])
+    result = [x1, y1, x2, y2]
+    duration = time.time() - start_time
+    print(result)
+    print(f"Performed lat/lon map in {duration} seconds")
+    (lon1, lat1) = wgs84_mapping.map_to_wgs84(x1, y1)
+    (lon2, lat2) = wgs84_mapping.map_to_wgs84(x2, y2)
+    map_back = [lon1, lat1, lon2, lat2]
+    print("Map back")
+    print(map_back)
+
+# Compare the grid bounds from a hydrogen domain path to test the mapping algorithm
+compare_domain_answer("/home/HYDROAPP/bill/wh3248/bill2/domain_state.json")
+
+
+    
+
---
+++ wgs84_maps/wgs84_maps.py
@@ -0,0 +1,100 @@
+"""
+Mapping from WGS84 lat/lon coordindates to Conus1/Conus2 coordindates using Geodesic mapping.
+"""
+
+import parflow as pf
+import json
+import numpy as np
+import time
+import math
+
+class WGS84Mapping:
+    """Methods to convert lat/lon to Conus1/Conus2 grid coordindates"""
+
+    def __init__(self):
+        """Constructor"""
+
+        self.lat_map = None
+        self.lon_map = None
+        self.grid_bounds = None
+        self.wgs84_bounds = None
+
+    def map_to_wgs84(self, x, y):
+        """Map an x,y conus grid point to lat long. Return [lon, lat]"""
+
+        lat = self.lat_map[y, x]
+        lon = self.lon_map[y, x]
+        return [lon, lat]
+
+    def map_to_grid(self, lon_point, lat_point):
+        """Map a lat/lon point and return a conus grid point (x,y)"""
+
+        nx = self.lat_map.shape[1]
+        ny = self.lat_map.shape[0]
+        row_indices = np.tile(np.matrix(np.linspace(0,ny-1,ny)).T, (1, nx))
+        col_indices = np.tile(np.matrix(np.linspace(0,nx-1,nx)), (ny, 1))
+
+        performance_option = True
+        if performance_option:
+            # subset the maps and indices to improve performance of closest
+            buffer_degrees = 2
+            conus_shape = self.lat_map.shape
+            mask_subset = np.ones(conus_shape)
+            mask_subset[self.lat_map < lat_point - buffer_degrees] = 0
+            mask_subset[self.lat_map > lat_point + buffer_degrees] = 0
+            mask_subset[self.lon_map < lon_point - buffer_degrees] = 0
+            mask_subset[self.lon_map > lon_point + buffer_degrees] = 0
+
+            subset_lat_map = self.lat_map[mask_subset>0]
+            subset_lon_map = self.lon_map[mask_subset>0]
+            subset_row_indices = np.squeeze(np.array(row_indices[mask_subset>0]))
+            subset_col_indices = np.squeeze(np.array(col_indices[mask_subset>0]))
+        else:
+            # Do not use performance option, use full maps in call to closest
+            subset_lat_map = self.lat_map
+            subset_lon_map = self.lon_map
+            subset_row_indices = np.squeeze(np.array(row_indices))
+            subset_col_indices = np.squeeze(np.array(col_indices))
+
+        result = self.closest(lat_point, lon_point, subset_lat_map, subset_lon_map, subset_row_indices, subset_col_indices)
+        return result
+
+    def load_maps(self, conus="conus1"):
+        """Load the conus1 or conus2 lat/lon map files"""
+
+        if conus.lower() == "conus1":
+            lat_map_path = "/home/HYDROAPP/common/shapefiles/CONUS1/Latitude_CONUS1.pfb"
+            lon_map_path = "/home/HYDROAPP/common/shapefiles/CONUS1/Longitude_CONUS1.pfb"
+        else:
+            lat_map_path = "/home/HYDROAPP/common/shapefiles/CONUS2/Latitude_CONUS2.pfb"
+            lon_map_path = "/home/HYDROAPP/common/shapefiles/CONUS2/Longitude_CONUS2.pfb"
+        self.lat_map = pf.read_pfb(lat_map_path)
+        self.lon_map = pf.read_pfb(lon_map_path)
+        self.lat_map = np.squeeze(self.lat_map)
+        self.lon_map = np.squeeze(self.lon_map)
+
+
+    def distance(self, lat1, lon1, lat2, lon2):
+        """Compute the distance between two lat/lon points using geodesic distance"""
+
+        p = math.pi/180
+        earth_diameter = 12742 # in meters
+        hav = 0.5 - np.cos((lat2-lat1)*p)/2 + np.cos(lat1*p)*np.cos(lat2*p) * (1-np.cos((lon2-lon1)*p)) / 2
+        return earth_diameter * np.arcsin(np.sqrt(hav))
+
+    def closest(self, lat_point, lon_point, lat_map, lon_map, row_indices, col_indices):
+        """
+            Find the closest conus index to the lat_point, lon_point. Return [x, y].
+
+            Use the lat_map, lon_map that map grid points to lat/lon points.
+            Use the row_indicies, col_indicies that contain the grid index.
+            The lat_map, lon_map, row_indices, col_indices have the same dimensions.
+            These dimensions could be subset for performance prior to calling this method.
+        """
+
+        dist_ = self.distance(lat_point,lon_point,lat_map,lon_map)
+
+        x = col_indices[dist_ == np.nanmin(dist_)]
+        y = row_indices[dist_ == np.nanmin(dist_)]
+        return (int(x), int(y))
+
