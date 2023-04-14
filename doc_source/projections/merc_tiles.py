6f305dbe04cff75b27200da56876f51aa24946f9
wh3248 <wh3248@princeton.edu>
2023-04-14 18:01:32 -0400

    update doc_source example with normal module docstring

--- doc_source/projections/merc_tiles.py
+++ doc_source/projections/merc_tiles.py
@@ -1,6 +1,4 @@
 """
-merc_tiles.py
-====================================
 Utility functions to support projections into Mercantile images.
 """
 
@@ -12,9 +10,10 @@ import PIL.Image
 import numpy as np
 import IPython.display
 
-def convert_conic_to_conus(conic:list):
+
+def convert_conic_to_conus(conic: list):
     """Convert from conic to Conus 1 coordinates.
-    
+
     Args:
         conus: An array of [x,y] in ESRI:10204 coordinates
 
@@ -27,13 +26,16 @@ def convert_conic_to_conus(conic:list):
     # Convert from meters to Kilometers and adjust to origin of conus1 on world map.
     x_origin = -1885055.4994999999180436
     y_origin = -604957.0653999999631196
-    x = int((x - x_origin)/1000)
-    y = int((y - y_origin)/1000)
+    x = int((x - x_origin) / 1000)
+    y = int((y - y_origin) / 1000)
     return (x, y)
 
-def convert_grid_to_conus(grid_x:int, grid_y:int, merc_bounds, conic_transformer, rect_size:int)->list:
+
+def convert_grid_to_conus(
+    grid_x: int, grid_y: int, merc_bounds, conic_transformer, rect_size: int
+) -> list:
     """Convert a mercantile x,y grid point to conus 1 x,y point.
-    
+
     Args:
         grid_x:             X coordinate within a mercantile tile.
         grid_y:             Y coordinate within a mercantile tile.
@@ -47,14 +49,17 @@ def convert_grid_to_conus(grid_x:int, grid_y:int, merc_bounds, conic_transformer
 
     lng_width = merc_bounds.east - merc_bounds.west
     lat_height = merc_bounds.north - merc_bounds.south
-    lat = merc_bounds.south + grid_y * lat_height/rect_size
-    lng = merc_bounds.west + grid_x * lng_width/rect_size
+    lat = merc_bounds.south + grid_y * lat_height / rect_size
+    lng = merc_bounds.west + grid_x * lng_width / rect_size
     (x, y) = convert_conic_to_conus(conic_transformer.transform(lat, lng))
     return (x, y)
 
-def convert_grid_from_conus(conus_x:int, conus_y:int, merc_bounds, transformer, rect_size:int=32)->list:
+
+def convert_grid_from_conus(
+    conus_x: int, conus_y: int, merc_bounds, transformer, rect_size: int = 32
+) -> list:
     """Convert a conus x,y point to a mercantile x,y point to reverse convert_grid_to_conus (debugging only).
-    
+
     Args:
         conus_x:    Conus X coordinate
     """
@@ -64,8 +69,10 @@ def convert_grid_from_conus(conus_x:int, conus_y:int, merc_bounds, transformer, 
     result_dist = 1000000
     for grid_x in range(0, rect_size):
         for grid_y in range(0, rect_size):
-            (x, y) = convert_grid_to_conus(grid_x, grid_y, merc_bounds, transformer, rect_size)
-            dist = (x - conus_x)**2 + (y - conus_y)**2
+            (x, y) = convert_grid_to_conus(
+                grid_x, grid_y, merc_bounds, transformer, rect_size
+            )
+            dist = (x - conus_x) ** 2 + (y - conus_y) ** 2
             if dist < result_dist:
                 if dist < 5:
                     print(x, y, dist)
@@ -73,6 +80,7 @@ def convert_grid_from_conus(conus_x:int, conus_y:int, merc_bounds, transformer, 
                 result_x = grid_x
                 result_y = grid_y
     return (result_x, result_y)
+
 
 def load_domain_mask(environment_name, user_id, domain_id):
     """Load a domain mask as a numpy array. Return the mask and the conus bounds."""
@@ -89,4 +97,4 @@ def load_domain_mask(environment_name, user_id, domain_id):
     domain_mask_path = f"{domain_path}/domain_files/domain_mask.png"
     mask_image = PIL.Image.open(domain_mask_path)
     mask_data = np.array(mask_image)
-    return(mask_data, grid_bounds)
+    return (mask_data, grid_bounds)
